d8d6821a4c855bdb2fcc40b45b176d6cffb8c4da
Naman Pancholi <mahendra.naman07@gmail.com>
2023-11-07 14:00:52 +0530

    database integrated

---
+++ storage.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+type Storage interface {
+	CreateAccount(*Account) error
+	DeleteAccount(int) error
+	UpdateAccount(*Account) error
+	GetAccount() ([])
+	GetAccountByID(int) (*Account, error)
+}
+
+type PostgresStore struct {
+	db *sql.DB
+}
+
+func NewPostgresStore() (*PostgresStore, error) {
+	constStr := "user=postgres dbname=postgres password=3007 sslmode=disable"
+	db, err := sql.Open("postgres", constStr)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return &PostgresStore{
+		db: db,
+	}, nil
+}
+
+func (s *PostgresStore) Init() error {
+	return s.CreateAccountTable()
+}
+
+func (s *PostgresStore) CreateAccountTable() error {
+	query := `create table if not exists account(
+    		id serial primary key, 
+    		first_name varchar(50), 
+    		last_name varchar(50),
+    		number serial,
+    		balance serial,
+    		created_at timestamp
+    )`
+	_, err := s.db.Exec(query)
+	return err
+}
+func (s *PostgresStore) CreateAccount(acc *Account) error {
+	query := `insert into account 
+			(first_name, last_name, number, balance, created_at)
+			values ($1, $2, $3, $4, $5)`
+
+	resp, err := s.db.Query(
+		query,
+		acc.FirstName,
+		acc.LastName,
+		acc.Number,
+		acc.Balance,
+		acc.CreatedAt)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%+v\n", resp)
+	return nil
+}
+func (s *PostgresStore) UpdateAccount(account *Account) error {
+	return nil
+}
+func (s *PostgresStore) DeleteAccount(id int) error {
+	return nil
+}
+func (s *PostgresStore) GetAccountByID(id int) (*Account, error) {
+	return nil, nil
+}
